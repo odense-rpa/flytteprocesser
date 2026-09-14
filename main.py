329b966349f990637a9f983a1e33f3bacbfa18df
Jakob Kirkegaard Wibroe <jkwibroe91@gmail.com>
2026-09-14 09:31:09 +0200

    stille start

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import asyncio
 import logging
 import sys
+from datetime import datetime, timedelta
 
 from automation_server_client import AutomationServer, Workqueue, WorkItemError, Credential, WorkItemStatus
+from eflyt_client import EflytClient
+from odk_tools.tracking import Tracker
+
+procesnavn = "Flytteprocesser"
+eflyt_client: EflytClient = None
+tracker: Tracker = None
 
 
-async def populate_queue(workqueue: Workqueue):
+def populate_queue(workqueue: Workqueue):
     logger = logging.getLogger(__name__)
 
     logger.info("Hello from populate workqueue!")
 
+    flyttedato_fra = (datetime.now() - timedelta(days=2)).strftime("%d-%m-%Y")
+    flyttedato_til = (datetime.now() + timedelta(days=4)).strftime("%d-%m-%Y")
+    flyttesager = eflyt_client.fremsøg_liste(flyttedato_fra=flyttedato_fra, flyttedato_til=flyttedato_til)
 
-async def process_workqueue(workqueue: Workqueue):
+    print("hej")
+
+
+def process_workqueue(workqueue: Workqueue):
     logger = logging.getLogger(__name__)
 
     logger.info("Hello from process workqueue!")
@@ -34,12 +46,22 @@ if __name__ == "__main__":
     workqueue = ats.workqueue()
 
     # Initialize external systems for automation here..
+    eflyt_credentials = Credential.get_credential("eFlyt")
+    tracking_credential = Credential.get_credential("Odense SQL Server")
+    eflyt_client = EflytClient(
+        base_url=eflyt_credentials.data["url"],
+        username=eflyt_credentials.username,
+        password=eflyt_credentials.password
+    )
 
+    tracker = Tracker(
+        username=tracking_credential.username, password=tracking_credential.password
+    )
     # Queue management
     if "--queue" in sys.argv:
         workqueue.clear_workqueue(WorkItemStatus.NEW)
-        asyncio.run(populate_queue(workqueue))
+        populate_queue(workqueue)
         exit(0)
 
     # Process workqueue
-    asyncio.run(process_workqueue(workqueue))
+    process_workqueue(workqueue)
